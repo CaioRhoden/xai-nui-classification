cd81afece3300622cdc434e64fdf7486fb7db436
Caio Emanuel Rhoden <caiorhoden@gmail.com>
2024-03-11 18:53:16 -0300

    hyperparameter tunning for binary experiment

--- src/models/xgboost_model.py
+++ src/models/xgboost_model.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('../../src')
 import pickle
 import optuna
-from sklearn.metrics import auc
+from sklearn.metrics import roc_auc_score
 import sklearn.metrics as metrics
 
 
@@ -27,26 +27,28 @@ class XGBoostModel(BaseModel):
     
 
 
-    def _objective(self, trial, y_pred_proba, y_true):
+    def _thrsh_objective(self, trial, y_pred_proba, y_true):
 
         threshold = trial.suggest_float("threshold", 0.0, 1.0)
 
         # Apply the threshold to your model's predictions
         y_pred_thresholded = (y_pred_proba >= threshold).astype(int)
 
-        # Calculate F1 Score
-        score = auc(y_true, y_pred_thresholded)
+        # Calculate ROCAU
+        score = roc_auc_score(y_true, y_pred_thresholded)
 
         return score
     
     def find_best_threshold(self, X, y):
         study = optuna.create_study(direction="maximize")
         y_pred_proba = self.model.predict_proba(X)[::,1]
-        study.optimize(lambda trial: self._objective(trial, y_pred_proba, y), n_trials=50)
+        study.optimize(lambda trial: self._thrsh_objective(trial, y_pred_proba, y), n_trials=50)
         best_threshold = study.best_trial.params['threshold']
         self.threshold = best_threshold
         return best_threshold
-        
+
+
+
     def predict(self, input_data):
 
         if self.threshold == 0.5:
--- src/tuning.py
+++ src/tuning.py
@@ -1,8 +1,109 @@
-from sklearn.model_selection import RandomizedSearchCV
+import optuna
+from sklearn.metrics import roc_auc_score
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier, Pool
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
-def random_search_tuning(model, parameters, random_state=42):
 
-    new_model = RandomizedSearchCV(estimator=model, param_distributions=parameters, cv=5, scoring='f1_weighted', random_state=random_state)
+def _objective_xgb(trial, x_train, y_train, x_val, y_val):
+    
+    params = {
+        "n_estimators": trial.suggest_int("n_estimators", 50, 150),
+        "verbosity": 0,
+        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.1),
+        "max_depth": trial.suggest_int("max_depth", 2, 8),
+        "subsample": trial.suggest_float("subsample", 0.3, 0.9),
+        "colsample_bytree": trial.suggest_float("colsample_bytree", 0.6, 1.0),
+        "min_child_weight": trial.suggest_int("min_child_weight", 1, 20),
+        "gamma": trial.suggest_float("gamma", 0, 5),
+        "eval_metric": "auc",
+    }
+    
+    xgb_clf = XGBClassifier(**params, random_state=42).fit(x_train, y_train, verbose=False)
+    y_pred = xgb_clf.predict_proba(x_val)
+    roc_auc = roc_auc_score(y_val, y_pred[:,1])
+    return roc_auc
 
+def _objective_cat(trial, x_train, y_train, x_val, y_val):
+    params = {
+        "learning_rate": trial.suggest_float("learning_rate", 1e-3, 0.1),
+        "depth": trial.suggest_int("max_depth", 2, 8),
+        "iterations": trial.suggest_int("n_estimators", 50, 150),
+        "l2_leaf_reg": trial.suggest_float("l2_leaf_reg", 0, 1),
+        }
 
-    return new_model
+    cat_clf = CatBoostClassifier(**params, random_state=42).fit(x_train, y_train, verbose=False)
+    y_pred = cat_clf.predict_proba(x_val)
+    roc_auc = roc_auc_score(y_val, y_pred[:,1])
+    return roc_auc
+
+def _objective_random_forest(trial, x_train, y_train, x_val, y_val):
+    params = {
+        "n_estimators": trial.suggest_int("n_estimators", 100, 1000),
+        "max_depth": trial.suggest_int("max_depth", 2, 8),
+        "min_samples_split": trial.suggest_int("min_samples_split", 2, 10),
+        "min_samples_leaf": trial.suggest_int("min_samples_leaf", 1, 10),
+        "bootstrap": trial.suggest_categorical("bootstrap", [True, False]),
+    }
+
+    rf_clf = RandomForestClassifier(**params, random_state=42).fit(x_train, y_train)
+    y_pred = rf_clf.predict_proba(x_val)
+    roc_auc = roc_auc_score(y_val, y_pred[:,1])
+    return roc_auc
+
+def _objective_lr(trial, x_train, y_train, x_val, y_val):
+    params = {
+        "solver": trial.suggest_categorical("solver", ["lbfgs", "sag", "saga", "liblinear", "newton-cg"]),
+        "max_iter": trial.suggest_int("max_iter", 100, 1000),
+        "C": trial.suggest_float("C", 0.01, 10.0),
+        "tol": trial.suggest_float("tol", 1e-6, 1e-2),
+    }
+
+    rf_clf = RandomForestClassifier(**params, random_state=42).fit(x_train, y_train)
+    y_pred = rf_clf.predict_proba(x_val)
+    roc_auc = roc_auc_score(y_val, y_pred[:,1])
+    return roc_auc
+
+def _objective_mlp(trial, x_train, y_train, x_val, y_val):
+    params = {
+        "hidden_layer_sizes": trial.suggest_int("hidden_layer_sizes", 10, 200),
+        "activation": trial.suggest_categorical("activation", ["identity", "logistic", "tanh", "relu"]),
+        "solver": trial.suggest_categorical("solver", ["lbfgs", "sgd", "adam"]),
+        "alpha": trial.suggest_float("alpha", 1e-5, 1e-1),
+        "learning_rate": trial.suggest_categorical("learning_rate", ["constant", "invscaling", "adaptive"]),
+        "early_stopping": trial.suggest_categorical("early_stopping", [True, False]),
+        "validation_fraction": trial.suggest_float("validation_fraction", 0.1, 0.3),
+    }
+
+    rf_clf = RandomForestClassifier(**params, random_state=42).fit(x_train, y_train)
+    y_pred = rf_clf.predict_proba(x_val)
+    roc_auc = roc_auc_score(y_val, y_pred[:,1])
+    return roc_auc
+
+def find_best_model(model, x_train, y_train, x_val, y_val, trials=100):
+
+    
+    study = optuna.create_study(direction="maximize")
+
+    if model == "xgb":
+        study.optimize(lambda trial: _objective_xgb(trial, x_train, y_train, x_val, y_val), n_trials=trials, show_progress_bar=True)
+    
+    elif model == "cat":
+        study.optimize(lambda trial: _objective_cat(trial, x_train, y_train, x_val, y_val), n_trials=trials, show_progress_bar=True)
+    
+    elif model == "random_forest":
+        study.optimize(lambda trial: _objective_random_forest(trial, x_train, y_train, x_val, y_val), n_trials=trials, show_progress_bar=True)
+
+    elif model == "lr":
+        study.optimize(lambda trial: _objective_lr(trial, x_train, y_train, x_val, y_val), n_trials=trials, show_progress_bar=True)
+    
+    elif model == "mlp":
+        study.optimize(lambda trial: _objective_mlp(trial, x_train, y_train, x_val, y_val), n_trials=trials, show_progress_bar=True)
+    
+    else:
+        raise("Model type not found")
+
+    
+    best_model = study.best_trial
+    return best_model
